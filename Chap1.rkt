9c485bb2352a9eecf408e305569bc69339c3d1a8
Justin Lee <justinlee0022@gmail.com>
2017-02-12 13:56:45 -0800

    completed q 1.8

--- Chap1.rkt
+++ Chap1.rkt
@@ -73,6 +73,24 @@ f
 |#
 
 ;;; 1.8
+(define (cbrt x) (cbrt-iter 1.0 x))
 
+(define (cbrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cbrt-iter (improve guess x) x)))
 
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (square x) (* x x))
+(define (cube x) (* x x x))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) 
+        (* 2 guess)) 
+     3))
+
+(cbrt 27)
+         
 (test)
